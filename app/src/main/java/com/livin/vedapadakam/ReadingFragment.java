4248797825333b431f7e32ef282922c57fdd6732
Livin Dcruz <livin.dcruz@sap.com>
2023-11-14 11:06:06 +0530

    9.1

--- app/src/main/java/com/livin/vedapadakam/ReadingFragment.java
+++ app/src/main/java/com/livin/vedapadakam/ReadingFragment.java
@@ -322,7 +322,7 @@ public class ReadingFragment extends Fragment {
         try {
 
             min = formatter.parse("14-5-2016");
-            max = formatter.parse("31-10-2023");
+            max = formatter.parse("29-2-2024");
             objDate= formatter.parse(date);
             if((objDate.compareTo(min)>=0 && objDate.compareTo(max)<=0 )||objDate.compareTo(formatter.parse("9-5-2016"))==0)
             {
@@ -338,7 +338,7 @@ public class ReadingFragment extends Fragment {
         }
         else if(withtoast)
         {
-            Toast.makeText(getActivity().getApplicationContext(), "Please enter a date between 14th May 2016 and 31st October  2023", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity().getApplicationContext(), "Please enter a date between 14th May 2016 and 29st February 2024", Toast.LENGTH_SHORT).show();
         }
         else{
             addUpdatefragment();
